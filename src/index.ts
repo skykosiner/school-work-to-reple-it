551c9b57d57818133c28777e5081e81adfb4beee
Yoni Kosiner <yoni@kosiner.co.uk>
2022-01-13 16:41:09 +0000

    Get file now works

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { exit } from "process";
 import askPath from "./askPath";
+import getFile from "./getFile";
 
 async function main() {
     let path = await askPath("What is the path to the file? ");
-
     path = "/home/yoni/school-work/" + path;
+
+    console.log(await getFile(path));
 
     exit(0);
 };
